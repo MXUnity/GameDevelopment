7aad2cb10cec8c124fcdc116ad7090dcf6374b5e
MXUnity <1378135984@qq.com>
2022-04-16 23:01:06 +0800

    Update Note

--- Assets/Pipeline/Deferred/CustomDRP.cs
+++ Assets/Pipeline/Deferred/CustomDRP.cs
@@ -8,7 +8,10 @@ public class CustomDRP : RenderPipeline
 {
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
+        for(int i =0; i < cameras.Length;i++)
+        {
 
+        }
         
         context.Submit();
     }
@@ -34,6 +37,57 @@ public class CustomDRP : RenderPipeline
         attachments[emissionIndex] = emission;
         context.BeginRenderPass(camera.pixelWidth, camera.pixelHeight, 1, attachments, depthIndex);
         attachments.Dispose();
+
+        // Start the first subpass, GBuffer creation: render to albedo, specRough, normal and emission, no need to read any input attachments
+        var gbufferColors = new NativeArray<int>(4, Allocator.Temp);
+        gbufferColors[0] = albedoIndex;
+        gbufferColors[1] = specRoughIndex;
+        gbufferColors[2] = normalIndex;
+        gbufferColors[3] = emissionIndex;
+        context.BeginSubPass(gbufferColors);
+        gbufferColors.Dispose();
+
+        // Render the deferred G-Buffer
+        // RenderGBuffer(cullResults, camera, context);
+        
+        context.EndSubPass();
+
+        // Second subpass, lighting: Render to the emission buffer, read from albedo, specRough, normal and depth.
+        // The last parameter indicates whether the depth buffer can be bound as read-only.
+        // Note that some renderers (notably iOS Metal) won't allow reading from the depth buffer while it's bound as Z-buffer,
+        // so those renderers should write the Z into an additional FP32 render target manually in the pixel shader and read from it instead
+        var lightingColors = new NativeArray<int>(1, Allocator.Temp);
+        lightingColors[0] = emissionIndex;
+        var lightingInputs = new NativeArray<int>(4, Allocator.Temp);
+        lightingInputs[0] = albedoIndex;
+        lightingInputs[1] = specRoughIndex;
+        lightingInputs[2] = normalIndex;
+        lightingInputs[3] = depthIndex;
+        context.BeginSubPass(lightingColors, lightingInputs, true);
+        lightingColors.Dispose();
+        lightingInputs.Dispose();
+
+        // PushGlobalShadowParams(context);
+        // RenderLighting(camera, cullResults, context);
+
+        context.EndSubPass();
+
+        // Third subpass, tonemapping: Render to albedo (which is bound to the camera target), read from emission.
+        var tonemappingColors = new NativeArray<int>(1, Allocator.Temp);
+        tonemappingColors[0] = albedoIndex;
+        var tonemappingInputs = new NativeArray<int>(1, Allocator.Temp);
+        tonemappingInputs[0] = emissionIndex;
+        context.BeginSubPass(tonemappingColors, tonemappingInputs, true);
+        tonemappingColors.Dispose();
+        tonemappingInputs.Dispose();
+
+        // present frame buffer.
+        // FinalPass(context);
+
+        context.EndSubPass();
+
+        context.EndRenderPass();
+
 
     }
     
